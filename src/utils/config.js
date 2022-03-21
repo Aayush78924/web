1dc3394724baca746cf9c2c1edc82f931a336f07
neobux78924 <47549234+Aayush78924@users.noreply.github.com>
2022-03-22 00:27:28 +0530

    .

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   debug: process.env.NODE_ENV === 'development',
 
-  siteName: '--siteName--',
+  siteName: 'Ecommerce',
   author: 'Parminder Klair',
   description: 'Premium Products Shop',
   logo: '/images/logo.png',
-  siteUrl: '--siteUrl--',
-  graphQlUri: 'https://site-api.now.sh/api',
-  graphQlUriDev: 'http://localhost:4000/api',
-  projectKey: '123123abc',
+  siteUrl: 'localhost:3000',
+  // graphQlUri: 'https://site-api.now.sh/api',
+  // graphQlUriDev: 'http://localhost:4000/api',
+  // projectKey: '123123abc',
 
   type: 'website',
   googleAnalytics: '',
   backgroundColor: '#e0e0e0',
   themeColor: '#E38D60',
-  sanityId: '--sanityId--',
-  razorPayKey: '',
-  mailchimp: '',
+  // sanityId: '--sanityId--',
+  // razorPayKey: '',
+  // mailchimp: '',
 };
